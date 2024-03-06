6aa93b3dcfe370c6d682733cd45dd2b1932d6c2f
Aryan shinde <shindearyan179@gmail.com>
2024-03-06 23:15:39 +0530

    Added Logout Button & Fixed Dashboard

--- app/umdashboard/page.jsx
+++ app/umdashboard/page.jsx
@@ -6,7 +6,15 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import { child, get, getDatabase, ref } from "firebase/database";
+import {
+  child,
+  get,
+  getDatabase,
+  orderByKey,
+  query,
+  ref,
+} from "firebase/database";
+import Link from "next/link";
 import { redirect, useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
@@ -27,7 +35,7 @@ const DashboardLogin = () => {
     }
   });
 
-  const signOutHandle = () => {
+  const signOutHandle = (e) => {
     e.preventDefault();
     signOut(auth)
       .then(() => {
@@ -56,12 +64,35 @@ const DashboardLogin = () => {
   };
 
   useEffect(() => {
+    // const fetchData = async () => {
+    //   const dbRef = ref(getDatabase());
+    //   get(child(dbRef, `unscrapMedia/contact/`)).then((snapshot) => {
+    //     if (snapshot.exists()) {
+    //       console.log(snapshot.val());
+    //       setData(snapshot.val());
+    //     } else {
+    //       console.log("No data available");
+    //     }
+    //   });
+    // };
+
     const fetchData = async () => {
       const dbRef = ref(getDatabase());
-      get(child(dbRef, `unscrapMedia/contact/`)).then((snapshot) => {
+      const queryRef = query(
+        child(dbRef, "unscrapMedia/contact/"),
+        orderByKey()
+      ); // Use orderByKey for sorting by ID
+
+      get(queryRef).then((snapshot) => {
         if (snapshot.exists()) {
-          console.log(snapshot.val());
-          setData(snapshot.val());
+          const data = snapshot.val();
+          // Convert the data object into an array for sorting
+          const dataArray = Object.values(data);
+          // Sort the array in descending order based on the key (ID)
+          dataArray.sort((a, b) => b.timestamp - a.timestamp); // Change to sort in descending order
+
+          console.log(dataArray);
+          setData(dataArray);
         } else {
           console.log("No data available");
         }
@@ -73,25 +104,42 @@ const DashboardLogin = () => {
 
   return (
     <div>
-      {user === null && <div>Loading...</div>}
+      {user === null && (
+        <div className="flex justify-center items-center min-h-[100svh] font-bold text-[24px] animate-pulse">
+          Loading...
+        </div>
+      )}
 
       {user === false && (
-        <div>
+        <div className="flex justify-center items-center min-h-[100svh]">
           <div>
-            <form>
+            <Link
+              href="/"
+              className="border-2 border-primary px-3 py-1 rounded hover:bg-primary hover:text-white transition-colors duration-200 ease-in-out text-primary font-semibold"
+            >
+              Go Home
+            </Link>
+            <form className="mt-6 flex flex-col gap-8 items-center border-2 px-6 py-10 rounded border-black/70">
               <input
                 type="text"
                 placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                className="w-[200px] md:w-[300px] border-2 border-black rounded px-3 py-1"
               />
               <input
                 type="password"
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                className="w-[200px] md:w-[300px] border-2 border-black rounded px-3 py-1"
               />
-              <button onClick={loginHandle}>Login</button>
+              <button
+                onClick={loginHandle}
+                className="border-2 border-primary px-4 py-2 hover:bg-primary hover:text-white transition-colors duration-200 ease-in-out rounded-md bg-white text-primary font-semibold"
+              >
+                Login
+              </button>
             </form>
           </div>
         </div>
@@ -100,53 +148,61 @@ const DashboardLogin = () => {
       {user === true && (
         <div className="min-h-[100svh] py-[3rem] md:py-[6rem] px-[2rem] flex justify-center items-center">
           <div className="max-w-7xl w-full flex flex-col md:flex-row justify-between items-center">
-            <ul role="list" className="divide-y divide-gray-100 w-full">
-              {data != null &&
-                Object.values(data)?.map((person) => (
-                  <li
-                    key={person.email}
-                    className="flex justify-between gap-x-6 py-5 cursor-pointer hover:bg-gray-200 transition-colors duration-200 ease-in-out px-4 rounded-lg"
-                    onClick={() =>
-                      router.push(`/umdashboard/${person.storedTimestamp}`)
-                    }
-                  >
-                    <div className="flex min-w-0 gap-x-4">
-                      <div className="min-w-0 flex-auto">
-                        <p className="text-sm font-semibold leading-6 text-gray-900">
-                          {person?.name}
-                        </p>
-                        <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                          {person.email}
-                        </p>
+            <div className="w-full">
+              <button
+                onClick={(e) => signOutHandle(e)}
+                className="border-2 border-primary px-4 py-2 hover:bg-primary hover:text-white transition-colors duration-200 ease-in-out rounded-md bg-white text-primary font-semibold mb-4"
+              >
+                Logout
+              </button>
+              <ul role="list" className="divide-y divide-gray-100 w-full">
+                {data != null &&
+                  Object.values(data)?.map((person) => (
+                    <li
+                      key={person.email}
+                      className="flex justify-between gap-x-6 py-5 cursor-pointer hover:bg-gray-200 transition-colors duration-200 ease-in-out px-4 rounded-lg"
+                      onClick={() =>
+                        router.push(`/umdashboard/${person.storedTimestamp}`)
+                      }
+                    >
+                      <div className="flex min-w-0 gap-x-4">
+                        <div className="min-w-0 flex-auto">
+                          <p className="text-sm font-semibold leading-6 text-gray-900">
+                            {person?.name}
+                          </p>
+                          <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                            {person.email}
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                      <p className="text-sm leading-6 text-gray-900">
-                        {new Date(person.timestamp).toLocaleDateString()}
-                      </p>
-                      {person.read ? (
-                        <div className="mt-1 flex items-center gap-x-1.5">
-                          <div className="flex-none rounded-full bg-gray-500/20 p-1">
-                            <div className="h-1.5 w-1.5 rounded-full bg-gray-500" />
+                      <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                        <p className="text-sm leading-6 text-gray-900">
+                          {new Date(person.timestamp).toLocaleDateString()}
+                        </p>
+                        {person.read ? (
+                          <div className="mt-1 flex items-center gap-x-1.5">
+                            <div className="flex-none rounded-full bg-gray-500/20 p-1">
+                              <div className="h-1.5 w-1.5 rounded-full bg-gray-500" />
+                            </div>
+                            <p className="text-xs leading-5 text-gray-500">
+                              {new Date(person.timestamp).toLocaleTimeString()}
+                            </p>
                           </div>
-                          <p className="text-xs leading-5 text-gray-500">
-                            {new Date(person.timestamp).toLocaleTimeString()}
-                          </p>
-                        </div>
-                      ) : (
-                        <div className="mt-1 flex items-center gap-x-1.5">
-                          <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                            <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                        ) : (
+                          <div className="mt-1 flex items-center gap-x-1.5">
+                            <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                              <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                            </div>
+                            <p className="text-xs leading-5 text-gray-500">
+                              {new Date(person.timestamp).toLocaleTimeString()}
+                            </p>
                           </div>
-                          <p className="text-xs leading-5 text-gray-500">
-                            {new Date(person.timestamp).toLocaleTimeString()}
-                          </p>
-                        </div>
-                      )}
-                    </div>
-                  </li>
-                ))}
-            </ul>
+                        )}
+                      </div>
+                    </li>
+                  ))}
+              </ul>
+            </div>
           </div>
           {/* <button onClick={signOutHandle}>SignOut</button> */}
           {/* <button onClick={getData}>GetData</button> */}
